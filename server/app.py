c0dd76ac16a4dc50c76f2de16bb137ba3c840cd5
shekinah <nyamburamary1212@gmail.com>
2024-01-12 10:08:37 +0300

    Complete validation flask sqlalchemy lab

--- server/app.py
+++ server/app.py
@@ -1,5 +1,11 @@
-from flask import Flask, make_response
+from flask import Flask
 from flask_migrate import Migrate
+import sqlalchemy
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import validates
 
 from models import db, EmailAddress
 
@@ -10,10 +16,37 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
+base = declarative_base()  # Add this line to define the base
 
 @app.route('/')
 def index():
     return 'Validations lab'
+
+class EmailAddress(base):
+    __tablename__ = 'address'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+
+    @validates('email')
+    def validate_email(self, key, address):
+        if '@' not in address:
+            raise ValueError("failed simple email validation")
+        return address
+
+Session = sessionmaker(db)
+session = Session()
+
+base.metadata.create_all(db)
+
+email = EmailAddress(email='banana')
+session.add(email)
+
+try:
+    session.commit()
+except sqlalchemy.exc.IntegrityError as e:
+    print("Integrity violation blocked!")
+    session.rollback()
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
